b21de6a06d526120aadf3f2396fefc7a854cbe9f
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-03-09 16:44:55 +0000

    Sqs remove test (#19)
    * Turn off listener test release
    
    * Turn off listener test release
    
    * Turn off listener test release
    
    * Turn off listener test release

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/DomainEventListenerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/DomainEventListenerService.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.servic
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import io.awspring.cloud.sqs.annotation.SqsListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.future.future
@@ -29,7 +28,7 @@ class DomainEventListenerService(
     val LOG: Logger = LoggerFactory.getLogger(this::class.java)
   }
 
-  @SqsListener(PRISON_VISITS_QUEUE_CONFIG_KEY, factory = "hmppsQueueContainerFactoryProxy")
+  // @SqsListener(PRISON_VISITS_QUEUE_CONFIG_KEY, factory = "hmppsQueueContainerFactoryProxy")
   fun onDomainEvent(
     rawMessage: String,
   ): CompletableFuture<Void> {
