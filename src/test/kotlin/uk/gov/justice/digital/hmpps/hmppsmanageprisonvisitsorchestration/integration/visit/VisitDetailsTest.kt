844a45a450ffe5c63f75b42b8cc3dcbe24abfaff
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-03-23 16:16:35 +0000

    VB-2028 - update test.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/visit/VisitDetailsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/visit/VisitDetailsTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpHeaders
 import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.client.VisitDetailsClient
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler.VisitDto
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.IntegrationTestBase
 
@@ -26,7 +27,8 @@ class VisitDetailsTest : IntegrationTestBase() {
     val reference = "aa-bb-cc-dd"
     val createdBy = "created-user"
     val lastUpdatedBy = "updated-user"
-    val visitDto = createVisitDto(reference = reference, createdBy = createdBy, updatedBy = lastUpdatedBy, cancelledBy = null)
+    val cancelledBy = "cancelled-user"
+    val visitDto = createVisitDto(reference = reference, createdBy = createdBy, updatedBy = lastUpdatedBy, cancelledBy = cancelledBy)
     visitSchedulerMockServer.stubGetVisit(reference, visitDto)
 
     // When
@@ -40,6 +42,55 @@ class VisitDetailsTest : IntegrationTestBase() {
     Assertions.assertThat(visitDtoResponse.createdByFullName).isEqualTo("$createdBy-name")
     Assertions.assertThat(visitDtoResponse.updatedBy).isEqualTo(lastUpdatedBy)
     Assertions.assertThat(visitDtoResponse.updatedByFullName).isEqualTo("$lastUpdatedBy-name")
+    Assertions.assertThat(visitDtoResponse.cancelledBy).isEqualTo(cancelledBy)
+    Assertions.assertThat(visitDtoResponse.cancelledByFullName).isEqualTo("$cancelledBy-name")
+  }
+
+  @Test
+  fun `when visit exists but userid is NOT_KNOWN search by reference returns the visit and names as NOT_KNOWN`() {
+    // Given
+    val reference = "aa-bb-cc-dd"
+    val createdBy = "NOT_KNOWN"
+    val lastUpdatedBy = "NOT_KNOWN"
+    val cancelledBy = "NOT_KNOWN"
+
+    val visitDto = createVisitDto(reference = reference, createdBy = createdBy, updatedBy = lastUpdatedBy, cancelledBy = cancelledBy)
+    visitSchedulerMockServer.stubGetVisit(reference, visitDto)
+
+    // When
+    val responseSpec = callVisitDetailsByReference(webTestClient, reference, roleVisitSchedulerHttpHeaders)
+
+    // Then
+    responseSpec.expectStatus().isOk
+    val visitDtoResponse = objectMapper.readValue(responseSpec.expectBody().returnResult().responseBody, VisitDto::class.java)
+    Assertions.assertThat(visitDtoResponse.reference).isEqualTo(visitDto.reference)
+    Assertions.assertThat(visitDtoResponse.createdBy).isEqualTo(createdBy)
+    Assertions.assertThat(visitDtoResponse.createdByFullName).isEqualTo(VisitDetailsClient.NOT_KNOWN)
+    Assertions.assertThat(visitDtoResponse.updatedBy).isEqualTo(lastUpdatedBy)
+    Assertions.assertThat(visitDtoResponse.updatedByFullName).isEqualTo(VisitDetailsClient.NOT_KNOWN)
+    Assertions.assertThat(visitDtoResponse.cancelledBy).isEqualTo(cancelledBy)
+    Assertions.assertThat(visitDtoResponse.cancelledByFullName).isEqualTo(VisitDetailsClient.NOT_KNOWN)
+  }
+
+  @Test
+  fun `when visit exists but userid is null search by reference returns the visit and names as null`() {
+    // Given
+    val reference = "aa-bb-cc-dd"
+    val createdBy = "invalid-user"
+    val visitDto = createVisitDto(reference = reference, createdBy = createdBy, updatedBy = null, cancelledBy = null)
+    visitSchedulerMockServer.stubGetVisit(reference, visitDto)
+
+    // When
+    val responseSpec = callVisitDetailsByReference(webTestClient, reference, roleVisitSchedulerHttpHeaders)
+
+    // Then
+    responseSpec.expectStatus().isOk
+    val visitDtoResponse = objectMapper.readValue(responseSpec.expectBody().returnResult().responseBody, VisitDto::class.java)
+    Assertions.assertThat(visitDtoResponse.reference).isEqualTo(visitDto.reference)
+    Assertions.assertThat(visitDtoResponse.createdBy).isEqualTo(createdBy)
+    Assertions.assertThat(visitDtoResponse.createdByFullName).isNull()
+    Assertions.assertThat(visitDtoResponse.updatedBy).isNull()
+    Assertions.assertThat(visitDtoResponse.updatedByFullName).isNull()
     Assertions.assertThat(visitDtoResponse.cancelledBy).isNull()
     Assertions.assertThat(visitDtoResponse.cancelledByFullName).isNull()
   }
