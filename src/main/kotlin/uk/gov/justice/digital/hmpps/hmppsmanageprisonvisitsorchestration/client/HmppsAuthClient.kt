f382208d9aabe2008b765bf59c36cc199357f34f
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-03-28 15:31:07 +0100

    VB-1926 Update orchestration service to acquire full username and return it to the UI for each entry, added error logging

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/client/HmppsAuthClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/client/HmppsAuthClient.kt
@@ -1,8 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.client
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.hmpps.auth.UserDetailsDto
@@ -11,13 +15,24 @@ import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.hmp
 class HmppsAuthClient(
   @Qualifier("hmppsAuthWebClient") private val webClient: WebClient,
 ) {
+
+  companion object {
+    val LOG: Logger = LoggerFactory.getLogger(this::class.java)
+  }
   fun getUserDetails(userName: String): Mono<UserDetailsDto> {
     return webClient.get()
       .uri("/api/user/$userName")
       .retrieve()
       .bodyToMono<UserDetailsDto>()
       .onErrorResume { e ->
-        return@onErrorResume Mono.just(UserDetailsDto(userName))
+        if (e is WebClientResponseException) {
+          if (e.statusCode != HttpStatus.NOT_FOUND) {
+            LOG.warn("Failed to acquire user information from auth $userName ", e)
+          }
+          return@onErrorResume Mono.just(UserDetailsDto(userName))
+        }
+        LOG.error("Failed to acquire user information from auth $userName ", e)
+        Mono.error(e)
       }
   }
 }
