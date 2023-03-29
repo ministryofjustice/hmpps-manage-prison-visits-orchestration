5d0b0e2f56b0bd83e98ae94ed29c32a1e99ddc28
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-03-29 15:00:17 +0100

    Update HmppsAuthClient.kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/client/HmppsAuthClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/client/HmppsAuthClient.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.client
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
