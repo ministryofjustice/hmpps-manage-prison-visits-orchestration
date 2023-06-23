91f100c016da1d0fa5e2cfac739af54378094d12
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-06-23 09:47:16 +0100

    Changes to Orchestration service for changes for - n weekly

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/SessionTimeSlotDto.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.visitscheduler.dto.sessions
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalTime
+
+class SessionTimeSlotDto(
+  @JsonFormat(pattern = "HH:mm", shape = JsonFormat.Shape.STRING)
+  @Schema(description = "The start time of the generated visit session(s)", example = "10:30", required = true)
+  val startTime: LocalTime,
+
+  @JsonFormat(pattern = "HH:mm", shape = JsonFormat.Shape.STRING)
+  @Schema(description = "The end time of the generated visit session(s)", example = "11:30", required = true)
+  val endTime: LocalTime,
+)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/enums/SessionTemplateFrequency.kt
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler.enums
-
-@Suppress("unused")
-enum class SessionTemplateFrequency {
-  BI_WEEKLY, WEEKLY, ONE_OFF
-}
