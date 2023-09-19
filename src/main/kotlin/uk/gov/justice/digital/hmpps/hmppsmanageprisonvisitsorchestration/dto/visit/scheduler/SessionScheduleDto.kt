1f719b1033e3b43c6162927cb0cae9a2d1f3bd9a
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-09-19 15:05:31 +0100

    Added @NotNull annotation against required fields.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/SessionScheduleDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/SessionScheduleDto.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.vi
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler.enums.VisitType
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.sessions.SessionDateRangeDto
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.sessions.SessionTimeSlotDto
@@ -14,18 +15,22 @@ data class SessionScheduleDto(
   val sessionTemplateReference: String,
 
   @Schema(description = "The time slot of the generated visit session(s)", required = true)
+  @NotNull
   val sessionTimeSlot: SessionTimeSlotDto,
 
   @Schema(description = "Validity period for the session template", required = true)
+  @NotNull
   val sessionDateRange: SessionDateRangeDto,
 
   @Schema(
     description = "The capacity for the session",
     required = true,
   )
+  @NotNull
   val capacity: SessionCapacityDto,
 
   @Schema(description = "visit type", example = "Social", required = true)
+  @NotNull
   val visitType: VisitType,
 
   @Schema(description = "prisoner location group", example = "Wing C", required = false)
@@ -38,5 +43,6 @@ data class SessionScheduleDto(
   val prisonerIncentiveLevelGroupNames: List<String>,
 
   @Schema(description = "number of weeks until the weekly day is repeated", example = "1", required = true)
+  @NotNull
   val weeklyFrequency: Int,
 )
