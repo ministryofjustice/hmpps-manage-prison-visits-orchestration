e06f975673676da3e09c31e5722bfe9757ca45b7
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-09-21 14:07:20 +0100

    VB-1536 Proactively manage bookings, added code to create DTO's and then call the visitSchedulerClient, added flag to turn off the DomainEventListenerService

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/listeners/events/EventFeatureSwitch.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/listeners/events/EventFeatureSwitch.kt
@@ -1,11 +1,20 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.service.listeners.events
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 
 @Component
-class EventFeatureSwitch(private val environment: Environment) {
+class EventFeatureSwitch(
+  private val environment: Environment,
+  @Value("\${hmpps.sqs.enabled:true}") private val allEventsEnabled: Boolean,
+) {
+
   fun isEnabled(eventType: String): Boolean {
-    return environment.getProperty("feature.event.$eventType", Boolean::class.java, true)
+    return isAllEventsEnabled() && environment.getProperty("feature.event.$eventType", Boolean::class.java, true)
+  }
+
+  fun isAllEventsEnabled(): Boolean {
+    return allEventsEnabled
   }
 }
