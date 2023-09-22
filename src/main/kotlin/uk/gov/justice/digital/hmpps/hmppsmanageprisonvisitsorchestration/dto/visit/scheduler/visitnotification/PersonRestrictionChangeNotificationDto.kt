2a4c1c72881c3b6e8f49d77fe9b527a2ff287a83
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-09-22 13:34:22 +0100

    VB-1536 Proactively manage bookings, removed json annotations as they were incorrectly added. (#96)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/visitnotification/PersonRestrictionChangeNotificationDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/visitnotification/PersonRestrictionChangeNotificationDto.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.vi
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include
-import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.service.listeners.events.additionalinfo.PersonRestrictionChangeInfo
 import java.time.LocalDate
@@ -10,17 +9,13 @@ import java.time.format.DateTimeFormatter
 
 data class PersonRestrictionChangeNotificationDto(
   @NotBlank
-  @JsonProperty("nomsNumber")
   val prisonerNumber: String,
   @NotBlank
-  @JsonProperty("contactPersonId")
   val visitorId: String,
   @NotBlank
-  @JsonProperty("effectiveDate")
   val validFromDate: LocalDate,
 
   @JsonInclude(Include.NON_NULL)
-  @JsonProperty("expiryDate")
   val validToDate: LocalDate? = null,
 ) {
 
