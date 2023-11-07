e98daa80e3d039e8c8e58cc8e68eecc555d662eb
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-11-07 16:24:21 +0000

    Merge develop into main no ff (#127)
    * VB-3054 Update non-association events to allow for new events types f… (#113)
    
    * VB-3054 Update non-association events to allow for new events types for NA service
    
    * VB-3054 Update non-association events to allow for new events types for NA service, should be @NotNull rather than NotBlank as it's an enum
    
    * Vb 3114 orchestration service changes to use the new endpoint as only name is needed (#115)
    
    * VB-3039 Proactive bookings : Create server side endpoint to get infor… (#116)
    
    * VB-3039 Proactive bookings : Create server side endpoint to get information back on the notification count
    
    * VB-3039 Proactive bookings : Create server side endpoint to get information back on the notification count
    
    * VB-3039 Proactive bookings : Create server side endpoint to get information back on the notification count
    
    * Merge main into develop (#119)
    
    * Dependency updates as a fix for security issues. (#117)
    
    * added a test job (#118)
    
    ---------
    
    Co-authored-by: Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
    Co-authored-by: anunagula3 <132376765+anunagula3@users.noreply.github.com>
    
    * Merge main into dev (#123)
    
    * Dependency updates as a fix for security issues. (#117)
    
    * added a test job (#118)
    
    * modified test runner (#120)
    
    ---------
    
    Co-authored-by: Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
    Co-authored-by: anunagula3 <132376765+anunagula3@users.noreply.github.com>
    
    * VB-3186 Changes to orchestration to allow us to run the correct tests against the correct code (#126)
    
    ---------
    
    Co-authored-by: Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
    Co-authored-by: anunagula3 <132376765+anunagula3@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/listeners/events/additionalinfo/NonAssociationChangedInfo.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/listeners/events/additionalinfo/NonAssociationChangedInfo.kt
@@ -1,21 +1,13 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.service.listeners.events.additionalinfo
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 
 data class NonAssociationChangedInfo(
   @NotBlank
-  @JsonProperty("nomsNumber")
+  @JsonProperty("nsPrisonerNumber1")
   val prisonerNumber: String,
   @NotBlank
-  @JsonProperty("nonAssociationNomsNumber")
+  @JsonProperty("nsPrisonerNumber2")
   val nonAssociationPrisonerNumber: String,
-  @NotBlank
-  @JsonProperty("effectiveDate")
-  val validFromDate: String,
-  @JsonInclude(Include.NON_NULL)
-  @JsonProperty("expiryDate")
-  val validToDate: String? = null,
 )
