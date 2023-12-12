6f3a0612154dafe6f20c27e2f8e83d27d38106d0
Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
2023-12-12 09:56:36 +0000

    Merge main into develop  (#145)
    * VB-3115 - set product ID to DPS035 (#131)
    
    * VB-3251 - update orchestration service to get user details from manage-users-api instead of hmpps-auth. (#135)
    
    * Reintroduce HMPPS_AUTH_URL as needed to retrieve auth token. (#137)
    
    * Update dependencies as a fix for security failures. (#140)
    
    * Migrating project to use groups in allowlist (#132)
    
    * Updating generic service version to 2.8
    
    * This PR migrates the project to use groups of IPs in their allowlist.
    
    By referring to groups to IP addresses, we can centralize the definition of groups of ip addresses.
    If these lists require changing in the future, we can change the definition once and future deploys across all services will automatically include these new IPs.
    
    1 allowlist(s) have been detected that can be migrated.
    
    
    
    ## Allowlist: helm_deploy/hmpps-manage-prison-visits-orchestration/values.yaml
    
    ### New Groups
    
    The effect of applying this PR is as follows:
    
    - The following groups will be applied: `internal`
    - The size of the allowlist defined in this file will change: `8 => 0 (8 removed)`
    
    ### Added IPs
    
    The new Group membership will result in the following IPs being added to your allowlist by applying this PR:
    
      Merging this PR should not result in any additional IP addresses being added to the allowlist.
    
    ### Removed IPs
    
    The following IPs have been identified as unnecessary and will be removed by applying this PR:
    
    
    - health-kick
    
    * Updating HMPPS circle orb version to 7
    
    * VB-3195 Update request methods to include applications made by a pris… (#141)
    
    * VB-3195 Update request methods to include applications made by a prisoner
    
    * VB-3195 Update request methods to include applications made by a prisoner
    
    * Update dependencies as a fix for security failures. (#143)
    
    * Update circle-ci orb version and jdk-tag. (#144)
    
    ---------
    
    Co-authored-by: Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
    Co-authored-by: Aled <101728271+AledWEvans@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.7.0"
-  kotlin("plugin.spring") version "1.9.10"
-  id("org.jetbrains.kotlin.plugin.noarg") version "1.9.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.10.1"
+  kotlin("plugin.spring") version "1.9.21"
+  id("org.jetbrains.kotlin.plugin.noarg") version "1.9.21"
 }
 
 configurations {
@@ -19,27 +19,27 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-cache")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.1.1")
   implementation("com.hazelcast:hazelcast:5.3.5")
   implementation("com.hazelcast:hazelcast-spring:5.3.5")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.30.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.32.0")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.3")
-  implementation("org.springframework.data:spring-data-commons:3.1.5")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.2.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-  implementation("org.springdoc:springdoc-openapi-starter-common:2.2.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.0")
+  implementation("org.springframework.data:spring-data-commons:3.2.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.3.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+  implementation("org.springdoc:springdoc-openapi-starter-common:2.3.0")
 
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.18")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.19")
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.19.1")
+  testImplementation("org.testcontainers:localstack:1.19.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -47,13 +47,13 @@ dependencies {
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(20))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "20"
+      jvmTarget = "21"
     }
   }
 }
