99c3574f85408e6e9e1d5f84c8befb11b9470f31
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-02-06 13:40:35 +0000

    Visit statues updated.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/enums/VisitStatus.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/enums/VisitStatus.kt
@@ -4,8 +4,6 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.vi
 enum class VisitStatus(
   val description: String,
 ) {
-  RESERVED("Reserved"),
-  CHANGING("Changing"),
   BOOKED("Booked"),
   CANCELLED("Cancelled"),
 }
