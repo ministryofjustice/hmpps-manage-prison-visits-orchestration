6a412f66856f0cef1ba530d03feb666d78dcd764
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-02-12 14:13:56 +0000

    Update dps-gradle-spring-boot version to 15.5.1 and associated ktlint fixes.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/mock/ManageUsersApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/mock/ManageUsersApiMockServer.kt
@@ -1,15 +1,13 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.mock
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.get
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 
-class ManageUsersApiMockServer(@Autowired private val objectMapper: ObjectMapper) : WireMockServer(8097) {
-  fun stubGetUserDetails(userId: String, fullName: String ? = "$userId-name") {
+class ManageUsersApiMockServer : WireMockServer(8097) {
+  fun stubGetUserDetails(userId: String, fullName: String? = "$userId-name") {
     val responseBuilder = aResponse()
       .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
 
