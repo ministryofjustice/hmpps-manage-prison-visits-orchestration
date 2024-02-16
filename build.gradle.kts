2451afa873b931ec6bfbdae6757a384c6fa2cf1f
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-02-16 15:36:43 +0000

    update dependencies and localstack versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.2"
   kotlin("plugin.spring") version "1.9.22"
   id("org.jetbrains.kotlin.plugin.noarg") version "1.9.22"
 }
@@ -19,17 +19,17 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-cache")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.1.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
   implementation("com.hazelcast:hazelcast:5.3.6")
   implementation("com.hazelcast:hazelcast-spring:5.3.6")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.0.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.1.0")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.1")
-  implementation("org.springframework.data:spring-data-commons:3.2.2")
+  implementation("org.springframework.data:spring-data-commons:3.2.3")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.3.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
   implementation("org.springdoc:springdoc-openapi-starter-common:2.3.0")
