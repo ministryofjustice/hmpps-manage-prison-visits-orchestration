a80c648d5ca0aa7c75facc2def40c93176545635
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2024-06-05 15:26:07 +0100

    added extra log message

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/ActionedByDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/ActionedByDto.kt
@@ -7,7 +7,7 @@ import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.vis
 @Schema(description = "Actioned By")
 class ActionedByDto(
 
-  @Schema(description = "booker reference", example = "asd-aed-vhj", required = true)
+  @Schema(description = "booker reference", example = "asd-aed-vhj", required = false)
   val bookerReference: String?,
 
   @Schema(description = "User Name", example = "AS/ALED", required = false)
