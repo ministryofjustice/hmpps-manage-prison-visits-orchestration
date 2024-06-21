4cb24ac013c0f851a5b3900ff7432db3c7e98ccf
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-06-21 09:26:21 +0100

    Updated variable names

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/utils/VisitBalancesUtil.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/utils/VisitBalancesUtil.kt
@@ -12,29 +12,28 @@ class VisitBalancesUtil(private val dateUtil: DateUtil) {
 
   fun calculateVoRenewalDate(visitBalance: VisitBalancesDto?): LocalDate {
     val currentDate = dateUtil.getCurrentDate()
-    val latestIepAdjustDate = calculateIepAdjustDate(visitBalance, currentDate)
-    val latestPrivilegedIepAdjustDate = calculatePrivilegedIepAdjustDate(visitBalance, currentDate)
+    val latestVORenewalDate = calculateVORenewalDate(visitBalance, currentDate)
+    val latestPVORenewalDate = calculatePVORenewalDate(visitBalance, currentDate)
 
-    return minOf(latestIepAdjustDate, latestPrivilegedIepAdjustDate)
+    return minOf(latestVORenewalDate, latestPVORenewalDate)
   }
 
-  private fun calculateIepAdjustDate(visitBalance: VisitBalancesDto?, dateFrom: LocalDate): LocalDate {
+  private fun calculateVORenewalDate(visitBalance: VisitBalancesDto?, currentDate: LocalDate): LocalDate {
     // IEP Adjust date is latestIepAdjustDate or if latestIepAdjustDate not available or today or in the past then calculated as TODAY + 14 days
-    val latestIepAdjustDate = getLatestAdjustDate(visitBalance?.latestIepAdjustDate, dateFrom)
-    return latestIepAdjustDate ?: dateFrom.plusDays(14)
+    val latestVORenewalDate = getLatestRenewalDate(visitBalance?.latestIepAdjustDate, currentDate)
+    return latestVORenewalDate ?: currentDate.plusDays(14)
   }
 
-  private fun calculatePrivilegedIepAdjustDate(visitBalance: VisitBalancesDto?, dateFrom: LocalDate): LocalDate {
+  private fun calculatePVORenewalDate(visitBalance: VisitBalancesDto?, currentDate: LocalDate): LocalDate {
     // Privileged IEP Adjust date is latestPrivIepAdjustDate or if latestPrivIepAdjustDate not available or today or in the past then calculated as 1st day of next month
-    getLatestAdjustDate(visitBalance?.latestIepAdjustDate, dateFrom)
-    val latestPrivIepAdjustDate = getLatestAdjustDate(visitBalance?.latestPrivIepAdjustDate, dateFrom)
-    return latestPrivIepAdjustDate ?: dateFrom.plusMonths(1).withDayOfMonth(1)
+    val latestPVORenewalDate = getLatestRenewalDate(visitBalance?.latestPrivIepAdjustDate, currentDate)
+    return latestPVORenewalDate ?: currentDate.plusMonths(1).withDayOfMonth(1)
   }
 
-  private fun getLatestAdjustDate(latestAdjustDate: LocalDate?, dateFrom: LocalDate): LocalDate? {
+  private fun getLatestRenewalDate(latestRenewalDate: LocalDate?, currentDate: LocalDate): LocalDate? {
     // ignore any dates that are before or equal to current date - dateFrom will always be current date
-    return latestAdjustDate?.let {
-      if (latestAdjustDate <= dateFrom) null else latestAdjustDate
+    return latestRenewalDate?.let {
+      if (latestRenewalDate <= currentDate) null else latestRenewalDate
     }
   }
 }
