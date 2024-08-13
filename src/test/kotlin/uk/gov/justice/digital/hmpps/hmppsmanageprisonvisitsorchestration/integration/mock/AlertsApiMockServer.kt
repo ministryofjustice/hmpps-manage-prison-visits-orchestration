325f8f82624da25a5284ae79cd8c0544a2367e5d
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2024-08-13 12:33:46 +0100

    VB-4121 - Move from Prison API to Alerts API for prisoner alerts (#271)
    * VB-4121 - Move from prison API to Alerts API for prisoneer alert codes
    
    Initial commit, introduce the new alerts client, and rework the
    prisoner profile and associated dto's to use the new client.
    
    * VB -4121 - Move from Prison API to Alerts API for prisoner alerts
    
    Move the dto over to alerts package and begin capturing fields from
    alerts api call
    
    * VB-4121 - Move from prison API to alerts API for alerts
    
    Add new Dtos to capture response from alerts API
    
    * VB-4121 - Move from prison API to to alerts API for prisoner alerts
    
    Cpature from API and convert into existing known Dto to avoid
    any changes needed on the frontend UI. In future if they require
    more information, this will allow us to more easily map between
    the API call and what we return.
    
    * fix unused imports and checkstyle issues
    
    * VB-4121 - Move from prison API to alerts API
    
    Update integration tests to mock new calls to alerts API
    
    * VB-4121 - Move from prison API to alerts API for alerts
    
    Move the alerts processing event to use the new alerts API. Later
    work on the visit scheduler side will remove any calls to prison API
    for alerts, as orchestration is handling it for the visit scheduler.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/mock/AlertsApiMockServer.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.mock
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.RestPage
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.alerts.api.AlertResponseDto
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.mock.MockUtils.Companion.getJsonString
+
+class AlertsApiMockServer : WireMockServer(9000) {
+  fun stubGetPrisonerAlertsMono(prisonerId: String, alertResponseDto: List<AlertResponseDto>) {
+    val totalElements = alertResponseDto.size
+    val restPage = RestPage(alertResponseDto, 1, 100, totalElements.toLong())
+    stubFor(
+      get("/prisoners/$prisonerId/alerts")
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .withStatus(200)
+            .withBody(
+              getJsonString(restPage),
+            ),
+        ),
+    )
+  }
+}
