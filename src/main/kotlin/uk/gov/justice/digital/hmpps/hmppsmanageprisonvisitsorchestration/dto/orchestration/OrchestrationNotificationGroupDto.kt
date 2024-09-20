c8a7bbfd60e7df65bfe482f3eacd85338c9301ca
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2024-09-20 11:45:47 +0100

    VB-4514 - Include additional information for visitor specific events
    Don't include passing it through to frontend yet as they UI work is coming
    in the future

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/orchestration/OrchestrationNotificationGroupDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/orchestration/OrchestrationNotificationGroupDto.kt
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
-import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler.enums.VisitorSupportedRestrictionType
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler.visitnotification.NotificationEventType
 
 class OrchestrationNotificationGroupDto(
@@ -17,11 +16,4 @@ class OrchestrationNotificationGroupDto(
   @Schema(description = "List of details of affected visits", required = true)
   @field:NotEmpty
   val affectedVisits: List<OrchestrationPrisonerVisitsNotificationDto>,
-  @Schema(description = "Description of the flagged event", example = "Visitor with id <id> has had restriction <restriction> added", required = false)
-  @field:NotBlank
-  val description: String? = null,
-  @Schema(description = "For visitor specific events, the id of the affected visitor", example = "1234567L", required = false)
-  val visitorId: Long? = null,
-  @Schema(description = "For visitor specific events, the restriction type of the affected visitor", example = "BAN", required = false)
-  val visitorRestrictionType: VisitorSupportedRestrictionType? = null,
 )
