51af87456e6c0dd508b2f3295510144df03031a4
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2024-11-01 09:39:44 +0000

    VB-4855 - Add userType to cancelVisitDto to help determine which user is making request
    Fixes bug where public users were being marked as STAFF in event_audit due to missing userType.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/CancelVisitDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/CancelVisitDto.kt
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.orchestration.CancelVisitOrchestrationDto
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler.enums.ApplicationMethodType
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler.enums.UserType
 
 data class CancelVisitDto(
   @field:Valid
@@ -17,10 +18,15 @@ data class CancelVisitDto(
   @Schema(description = "application method", required = true)
   @field:NotNull
   val applicationMethodType: ApplicationMethodType,
+
+  @Schema(description = "User type", example = "STAFF", required = true)
+  @field:NotNull
+  val userType: UserType,
 ) {
   constructor(cancelVisitOrchestrationDto: CancelVisitOrchestrationDto) : this (
     cancelOutcome = cancelVisitOrchestrationDto.cancelOutcome,
     actionedBy = cancelVisitOrchestrationDto.actionedBy,
     applicationMethodType = cancelVisitOrchestrationDto.applicationMethodType,
+    userType = cancelVisitOrchestrationDto.userType,
   )
 }
