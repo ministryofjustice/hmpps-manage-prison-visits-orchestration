66e01092b189699d7ed942fde8fcb809c8211a43
Tom Watkins <tom.watkins@justice.gov.uk>
2025-03-20 20:45:27 +0000

    Added logging

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/VisitWriteEventListenerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/VisitWriteEventListenerService.kt
@@ -5,7 +5,10 @@ import io.awspring.cloud.sqs.annotation.SqsListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.future.future
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.service.DomainEventListenerService.Companion
 import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.service.listeners.events.VisitWriteEvent
 import java.util.concurrent.CompletableFuture
 
@@ -15,16 +18,26 @@ const val PRISON_VISITS_WRITES_QUEUE_CONFIG_KEY = "prisonvisitswriteevents"
 class VisitWriteEventListenerService(
   val objectMapper: ObjectMapper,
 ) {
+  private companion object {
+    val LOG: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
   @SqsListener(PRISON_VISITS_WRITES_QUEUE_CONFIG_KEY, factory = "hmppsQueueContainerFactoryProxy")
   fun onEventReceived(
     rawMessage: String,
   ): CompletableFuture<Void> = asCompletableFuture {
-    val sqsMessage = objectMapper.readValue(rawMessage, VisitWriteEvent::class.java)
-    when(sqsMessage.eventType) {
-      "VisitCreated" -> {}
-      "VisitUpdated" -> {}
-      "VisitCancelled" -> {}
-      else -> throw Exception("Cannot process event of type ${sqsMessage.eventType}")
+    try {
+      LOG.debug("Received visit write event: $rawMessage")
+      val sqsMessage = objectMapper.readValue(rawMessage, VisitWriteEvent::class.java)
+      when(sqsMessage.eventType) {
+        "VisitCreated" -> {}
+        "VisitUpdated" -> {}
+        "VisitCancelled" -> {}
+        else -> throw Exception("Cannot process event of type ${sqsMessage.eventType}")
+      }
+    } catch (e: Exception) {
+      LOG.error("Error processing visit write event", e)
+      throw e
     }
   }
 }
