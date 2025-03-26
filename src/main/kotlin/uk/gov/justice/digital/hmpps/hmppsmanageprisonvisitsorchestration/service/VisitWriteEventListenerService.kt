54fa7942ec39f6a97a4314005c1baaf7b4f909d7
Tom Watkins <tom.watkins@justice.gov.uk>
2025-03-26 11:37:39 +0000

    Add service unit tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/VisitWriteEventListenerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/VisitWriteEventListenerService.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.service
 
+import com.fasterxml.jackson.databind.DatabindException
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.awspring.cloud.sqs.annotation.SqsListener
 import kotlinx.coroutines.CoroutineScope
@@ -14,12 +15,12 @@ import java.util.concurrent.CompletableFuture
 const val PRISON_VISITS_WRITES_QUEUE_CONFIG_KEY = "prisonvisitswriteevents"
 
 @Service
-class VisitWriteEventListenerService {
+class VisitWriteEventListenerService(
+  private val objectMapper: ObjectMapper,
+) {
   private companion object {
     val LOG: Logger = LoggerFactory.getLogger(this::class.java)
   }
-
-  private val objectMapper = ObjectMapper()
 
   @SqsListener(PRISON_VISITS_WRITES_QUEUE_CONFIG_KEY, factory = "hmppsQueueContainerFactoryProxy")
   fun onEventReceived(
