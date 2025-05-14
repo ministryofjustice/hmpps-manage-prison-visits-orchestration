26145a655a9f0326f8cc4a9fb1c0022b166ec6f7
Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
2025-05-14 13:21:13 +0100

    Vb 5325 improve the way visit notification events are created for non associations (#426)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/enums/NotificationEventAttributeType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/enums/NotificationEventAttributeType.kt
@@ -4,4 +4,5 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.vi
 enum class NotificationEventAttributeType {
   VISITOR_RESTRICTION,
   VISITOR_ID,
+  PAIRED_VISIT,
 }
