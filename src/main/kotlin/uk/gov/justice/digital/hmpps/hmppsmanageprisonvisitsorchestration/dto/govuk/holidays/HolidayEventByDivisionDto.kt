bc2eea6d4256594f0148350df08555df461848a3
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-07-01 14:05:12 +0100

    VB-5685 - update required flag on Schema annotation.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/govuk/holidays/HolidayEventByDivisionDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/govuk/holidays/HolidayEventByDivisionDto.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.go
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class HolidayEventByDivisionDto(
-  @param:Schema(description = "division", example = "england-and-wales", required = true)
+  @param:Schema(description = "division", example = "england-and-wales", required = false)
   val division: String?,
 
   @param:Schema(description = "holiday events", required = true)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/govuk/holidays/HolidayEventDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/govuk/holidays/HolidayEventDto.kt
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
 data class HolidayEventDto(
-  @param:Schema(required = true, description = "Holiday title", example = "New Year’s Day")
+  @param:Schema(description = "Holiday title", example = "New Year’s Day", required = false)
   val title: String?,
 
-  @param:Schema(required = true, description = "Holiday date", example = "2026-01-01")
+  @param:Schema(description = "Holiday date", example = "2026-01-01", required = true)
   val date: LocalDate,
 )
