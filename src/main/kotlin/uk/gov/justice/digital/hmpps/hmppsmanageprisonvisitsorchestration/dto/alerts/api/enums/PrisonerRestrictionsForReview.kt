eef03b959415c21469a010a5a58c8d661a074910
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-07-04 09:51:36 +0100

    revert VB-5751 - changes from main as it needs to go into a feature branch.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/alerts/api/enums/PrisonerRestrictionsForReview.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.alerts.api.enums
+
+enum class PrisonerRestrictionsForReview {
+  CHILD,
+  NONCON,
+  PREINF,
+  RESTRICT,
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/alerts/api/enums/VisitorRestrictionsForReview.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.alerts.api.enums
+
+enum class VisitorRestrictionsForReview {
+  BAN,
+  CHILD,
+  CLOSED,
+  NONCON,
+  PREINF,
+  RESTRICT,
+}
