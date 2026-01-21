f2b83f1632a941fcda5cc8c6a42535bbbe1e54d6
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-01-21 10:48:12 +0000

    Minor update to tests.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/TestObjectMapper.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/TestObjectMapper.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integr
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.kotlinModule
@@ -14,5 +15,6 @@ object TestObjectMapper {
       .defaultPropertyInclusion(
         JsonInclude.Value.construct(JsonInclude.Include.NON_NULL, JsonInclude.Include.NON_NULL),
       )
+      .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
       .build()
 }
