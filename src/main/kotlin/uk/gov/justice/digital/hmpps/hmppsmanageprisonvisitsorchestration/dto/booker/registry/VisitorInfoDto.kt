ec00393d21c09408baa34c17f0db2a7a558025fd
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-01-30 11:46:29 +0000

    shorten name

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/booker/registry/VisitorInfoDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/booker/registry/VisitorInfoDto.kt
@@ -17,15 +17,15 @@ data class VisitorInfoDto(
   val dateOfBirth: LocalDate?,
   @param:Schema(description = "Relevant visitor restrictions that impact visits or empty list if none", required = true)
   val visitorRestrictions: Set<VisitorRestrictionDto>,
-  @param:Schema(description = "A flag to signify if the visitor is on the approved or unapproved social contact list", example = "true", required = true)
-  val approvedVisitor: Boolean,
+  @param:Schema(description = "A flag to signify if the visitor is on the approved social contact list", example = "true", required = true)
+  val approved: Boolean,
 ) {
   constructor(contact: PrisonerContactDto, visitorRestrictions: Set<VisitorRestrictionDto>) : this(
     visitorId = contact.personId!!,
     firstName = contact.firstName,
     lastName = contact.lastName,
     dateOfBirth = contact.dateOfBirth,
-    approvedVisitor = contact.approvedVisitor,
+    approved = contact.approvedVisitor,
     visitorRestrictions = visitorRestrictions,
   )
 }
