73c4959b12908a425c941dec85a032b1d4c269c5
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-02-02 10:04:45 +0000

    VB-5979 - Part 2 (return unapproved visitors on a bookers account too)
    Instead of only allowing approved linked visitors, we will now return
    the unapproved linked visitors too. The frontend will display these to
    the booker, to show them the visitor is still linked, but cannot be booked
    for.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/PrisonerContactService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/service/PrisonerContactService.kt
@@ -20,6 +20,11 @@ class PrisonerContactService(
     return prisonerContactRegistryClient.getPrisonersApprovedSocialContacts(prisonerNumber, withAddress = false, hasDateOfBirth = true)
   }
 
+  fun getPrisonersSocialContactsWithDOB(prisonerNumber: String): List<PrisonerContactDto> {
+    LOG.debug("Getting full social contacts list (with DOB) for prisoner - {}", prisonerNumber)
+    return prisonerContactRegistryClient.getPrisonersSocialContacts(prisonerNumber, withAddress = false, hasDateOfBirth = true)
+  }
+
   fun getPrisonersContacts(prisonerIds: Set<String>): Map<String, List<PrisonerContactDto>> {
     val prisonersContactMap = mutableMapOf<String, List<PrisonerContactDto>>()
     prisonerIds.forEach { prisonerId ->
