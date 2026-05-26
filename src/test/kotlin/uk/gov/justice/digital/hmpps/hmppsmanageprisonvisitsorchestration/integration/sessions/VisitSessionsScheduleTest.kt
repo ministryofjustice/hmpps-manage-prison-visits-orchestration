ad09c180e2b237b13307ef941444f0de87b3e535
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-05-26 10:16:49 +0100

    Minor update

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/sessions/VisitSessionsScheduleTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/sessions/VisitSessionsScheduleTest.kt
@@ -153,7 +153,6 @@ class VisitSessionsScheduleTest : IntegrationTestBase() {
     includeExcludedSessions?.let {
       uri += "&includeExcludedSessions=$includeExcludedSessions"
     }
-    println(uri)
     return webTestClient.get().uri(uri)
       .headers(authHttpHeaders)
       .exchange()
