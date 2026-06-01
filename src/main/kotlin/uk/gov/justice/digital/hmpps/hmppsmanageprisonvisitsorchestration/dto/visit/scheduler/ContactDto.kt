f2e4e675207732292429987508d2c931b5a09820
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-06-01 11:51:30 +0100

    VB-6593 - Allow main contact to select a language preference for their communications
    Add new field to contact dto. For now we'll supported EN / CY.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/ContactDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/ContactDto.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.vi
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.v3.oas.annotations.media.Schema
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.booker.registry.enums.LanguagePreference
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(description = "Contact")
@@ -12,6 +13,8 @@ open class ContactDto(
   open val telephone: String? = null,
   @param:Schema(description = "Contact Email Address", example = "email@example.com", required = false)
   open val email: String? = null,
+  @param:Schema(name = "languagePreference", description = "The language in which your correspondence will be sent", defaultValue = "EN", required = false)
+  open val languagePreference: LanguagePreference = LanguagePreference.EN,
 ) {
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
@@ -22,6 +25,7 @@ open class ContactDto(
     if (name != other.name) return false
     if (telephone != other.telephone) return false
     if (email != other.email) return false
+    if (languagePreference != other.languagePreference) return false
 
     return true
   }
@@ -30,8 +34,9 @@ open class ContactDto(
     var result = name.hashCode()
     result = 31 * result + (telephone?.hashCode() ?: 0)
     result = 31 * result + (email?.hashCode() ?: 0)
+    result = 31 * result + (languagePreference.hashCode())
     return result
   }
 
-  override fun toString(): String = "ContactDto(name='$name', telephone=$telephone, email=$email)"
+  override fun toString(): String = "ContactDto(name='$name', telephone=$telephone, email=$email, language preference=$languagePreference)"
 }
