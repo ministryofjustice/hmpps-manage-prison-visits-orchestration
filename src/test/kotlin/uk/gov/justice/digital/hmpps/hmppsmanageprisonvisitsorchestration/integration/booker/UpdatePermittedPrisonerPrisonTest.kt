e90e82b3bfd062d690953142c109082b74f6543a
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-07-02 13:37:55 +0100

    VB-5864 - Allow booker to update prisoners location
    When a prisoner is transferred the booker can no longer book for them
    at their original registered location. This PR allows the booker to
    update the prisoner's location, to re-enable booking.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/booker/UpdatePermittedPrisonerPrisonTest.kt
@@ -0,0 +1,133 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.booker
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.controller.PUBLIC_BOOKER_UPDATE_PERMITTED_PRISONER_PRISON_CONTROLLER_PATH
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.booker.registry.PermittedPrisonerForBookerDto
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.booker.registry.UpdateRegisteredPrisonersPrisonDto
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.TestObjectMapper
+
+@DisplayName("$PUBLIC_BOOKER_UPDATE_PERMITTED_PRISONER_PRISON_CONTROLLER_PATH - Update a permitted prisoner's registered prison")
+class UpdatePermittedPrisonerPrisonTest : IntegrationTestBase() {
+  @Test
+  fun `when update permitted prisoner prison is successful HTTP status OK and updated prisoner are returned`() {
+    // Given
+    val bookerReference = "booker-reference"
+    val prisonerId = "AA12345"
+    val updateRegisteredPrisonersPrisonDto = UpdateRegisteredPrisonersPrisonDto("MDI")
+    val response = PermittedPrisonerForBookerDto(
+      prisonerId = prisonerId,
+      prisonCode = updateRegisteredPrisonersPrisonDto.prisonCode,
+      permittedVisitors = emptyList(),
+    )
+
+    prisonVisitBookerRegistryMockServer.stubUpdatePermittedPrisonerPrison(
+      bookerReference = bookerReference,
+      prisonerId = prisonerId,
+      updateRegisteredPrisonersPrisonDto = updateRegisteredPrisonersPrisonDto,
+      response = response,
+      httpStatus = HttpStatus.OK,
+    )
+
+    // When
+    val responseSpec = callUpdatePermittedPrisonerPrison(bookerReference, prisonerId, updateRegisteredPrisonersPrisonDto, webTestClient, roleVSIPOrchestrationServiceHttpHeaders)
+
+    // Then
+    responseSpec.expectStatus().isOk
+    val updatedPrisoner = getResponse(responseSpec)
+    assertThat(updatedPrisoner.prisonerId).isEqualTo(prisonerId)
+    assertThat(updatedPrisoner.prisonCode).isEqualTo(updateRegisteredPrisonersPrisonDto.prisonCode)
+    verify(prisonVisitBookerRegistryClientSpy, times(1)).updatePermittedPrisonerPrison(bookerReference, prisonerId, updateRegisteredPrisonersPrisonDto)
+  }
+
+  @Test
+  fun `when booker registry returns a bad request then it is returned to caller`() {
+    // Given
+    val bookerReference = "booker-reference"
+    val prisonerId = "AA12345"
+    val updateRegisteredPrisonersPrisonDto = UpdateRegisteredPrisonersPrisonDto("MDI")
+
+    prisonVisitBookerRegistryMockServer.stubUpdatePermittedPrisonerPrison(
+      bookerReference = bookerReference,
+      prisonerId = prisonerId,
+      updateRegisteredPrisonersPrisonDto = updateRegisteredPrisonersPrisonDto,
+      httpStatus = HttpStatus.BAD_REQUEST,
+    )
+
+    // When
+    val responseSpec = callUpdatePermittedPrisonerPrison(bookerReference, prisonerId, updateRegisteredPrisonersPrisonDto, webTestClient, roleVSIPOrchestrationServiceHttpHeaders)
+
+    // Then
+    responseSpec.expectStatus().isBadRequest
+    verify(prisonVisitBookerRegistryClientSpy, times(1)).updatePermittedPrisonerPrison(bookerReference, prisonerId, updateRegisteredPrisonersPrisonDto)
+  }
+
+  @Test
+  fun `when booker registry returns not found then it is returned to caller`() {
+    // Given
+    val bookerReference = "booker-reference"
+    val prisonerId = "AA12345"
+    val updateRegisteredPrisonersPrisonDto = UpdateRegisteredPrisonersPrisonDto("MDI")
+
+    prisonVisitBookerRegistryMockServer.stubUpdatePermittedPrisonerPrison(
+      bookerReference = bookerReference,
+      prisonerId = prisonerId,
+      updateRegisteredPrisonersPrisonDto = updateRegisteredPrisonersPrisonDto,
+      httpStatus = HttpStatus.NOT_FOUND,
+    )
+
+    // When
+    val responseSpec = callUpdatePermittedPrisonerPrison(bookerReference, prisonerId, updateRegisteredPrisonersPrisonDto, webTestClient, roleVSIPOrchestrationServiceHttpHeaders)
+
+    // Then
+    responseSpec.expectStatus().isNotFound
+    verify(prisonVisitBookerRegistryClientSpy, times(1)).updatePermittedPrisonerPrison(bookerReference, prisonerId, updateRegisteredPrisonersPrisonDto)
+  }
+
+  @Test
+  fun `when update permitted prisoner prison is called with no auth request is rejected`() {
+    // Given
+    val invalidRoleHttpHeaders = setAuthorisation(roles = listOf("ROLE_INVALID"))
+
+    // When
+    val responseSpec = callUpdatePermittedPrisonerPrison(
+      "booker-reference",
+      "AA12345",
+      UpdateRegisteredPrisonersPrisonDto("MDI"),
+      webTestClient,
+      invalidRoleHttpHeaders,
+    )
+
+    // Then
+    responseSpec.expectStatus().isForbidden
+    verify(prisonVisitBookerRegistryClientSpy, times(0)).updatePermittedPrisonerPrison(any(), any(), any())
+  }
+
+  private fun getResponse(responseSpec: WebTestClient.ResponseSpec): PermittedPrisonerForBookerDto = TestObjectMapper.mapper.readValue(responseSpec.expectBody().returnResult().responseBody, PermittedPrisonerForBookerDto::class.java)
+
+  private fun callUpdatePermittedPrisonerPrison(
+    bookerReference: String,
+    prisonerId: String,
+    updateRegisteredPrisonersPrisonDto: UpdateRegisteredPrisonersPrisonDto,
+    webTestClient: WebTestClient,
+    authHttpHeaders: (HttpHeaders) -> Unit,
+  ): WebTestClient.ResponseSpec {
+    val uri = PUBLIC_BOOKER_UPDATE_PERMITTED_PRISONER_PRISON_CONTROLLER_PATH
+      .replace("{bookerReference}", bookerReference)
+      .replace("{prisonerId}", prisonerId)
+
+    return webTestClient.put().uri(uri)
+      .headers(authHttpHeaders)
+      .body(BodyInserters.fromValue(updateRegisteredPrisonersPrisonDto))
+      .exchange()
+  }
+}
