613ddb7d6a551f833746b7adcf46530c25e33149
Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
2026-07-08 11:59:23 +0100

    Update get future session blocked dates (#752)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/prisons/PrisonAndSessionsExcludeDatesDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/prisons/PrisonAndSessionsExcludeDatesDto.kt
@@ -6,6 +6,6 @@ data class PrisonAndSessionsExcludeDatesDto(
   @param:Schema(description = "Dates excluded for visits (full day exclusions), empty if none.", required = true)
   val fullDateExclusions: List<ExcludeDateDto>,
 
-  @param:Schema(description = "Map of session template reference and list of dates for which the session has been excluded for visits, empty if none.", required = true)
-  val sessionExclusions: Map<String, List<ExcludeDateDto>>,
+  @param:Schema(description = "List of sessions that have future exclusions, empty if none.", required = true)
+  val sessionExclusions: List<SessionExcludeDateDto>,
 )
