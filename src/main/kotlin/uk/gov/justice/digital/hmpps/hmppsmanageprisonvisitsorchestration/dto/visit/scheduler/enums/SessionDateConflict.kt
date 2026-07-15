88a6084c4f65e9a8a29c505305f7558c48d01841
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-07-15 13:00:58 +0100

    Code review updates

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/enums/SessionDateConflict.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/enums/SessionDateConflict.kt
@@ -13,7 +13,9 @@ enum class SessionDateConflict {
       SessionConflict.NON_ASSOCIATION -> NON_ASSOCIATION
       SessionConflict.PRISON_DATE_BLOCKED -> PRISON_DATE_BLOCKED
       SessionConflict.REMAND_VISITS_LIMIT_REACHED -> REMAND_VISITS_LIMIT_REACHED
+      // as a SESSION_DATE_BLOCKED conflict is specific to the session and does not affect the whole date, we set it to null
       SessionConflict.SESSION_DATE_BLOCKED -> null
+      // as a DOUBLE_BOOKING_OR_RESERVATION conflict is specific to the session and does not affect the whole date, we set it to null
       SessionConflict.DOUBLE_BOOKING_OR_RESERVATION -> null
     }
   }
