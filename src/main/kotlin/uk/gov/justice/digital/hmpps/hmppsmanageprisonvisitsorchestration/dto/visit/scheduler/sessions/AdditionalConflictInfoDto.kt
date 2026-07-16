b456396fc91948c4ec5cd3d96adcb8123ff55bdb
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-07-16 12:32:40 +0100

    add type safety to orchestration

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/sessions/AdditionalConflictInfoDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/sessions/AdditionalConflictInfoDto.kt
@@ -2,11 +2,13 @@ package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.vi
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler.enums.SessionConflictAttribute
 
 data class AdditionalConflictInfoDto(
   @param:Schema(description = "Attribute Name", required = true)
-  @field:NotBlank
-  val attributeName: String,
+  @field:NotNull
+  val attributeName: SessionConflictAttribute,
 
   @param:Schema(description = "Attribute value", required = true)
   @field:NotBlank
