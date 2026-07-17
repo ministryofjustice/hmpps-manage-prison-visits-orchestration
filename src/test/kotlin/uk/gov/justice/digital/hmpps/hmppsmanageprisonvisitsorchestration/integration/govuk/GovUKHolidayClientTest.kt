0c61701fd9f7058b7ad1c1e2d64ce9a33b25e3df
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-07-17 09:55:27 +0100

    VB-6835 - 3rd party API calls use unauthenticated web clients
    More appropriate flow for a call to a 3rd party external API as
    it doesn't require the usual internal auth.
    
    Added test class to assert the call made doesn't contain an auth header.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/integration/govuk/GovUKHolidayClientTest.kt
@@ -0,0 +1,41 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.govuk
+
+import com.github.tomakehurst.wiremock.client.WireMock
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.CacheManager
+import org.springframework.http.HttpHeaders
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.client.GovUKHolidayClient
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.client.GovUKHolidayClient.Companion.HOLIDAYS_JSON
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.govuk.holidays.HolidayEventByDivisionDto
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.govuk.holidays.HolidaysDto
+import uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.integration.IntegrationTestBase
+
+class GovUKHolidayClientTest : IntegrationTestBase() {
+  @Autowired
+  lateinit var govUKHolidayClient: GovUKHolidayClient
+
+  @Autowired
+  lateinit var cacheManager: CacheManager
+
+  @BeforeEach
+  fun clearBankHolidaysCache() {
+    cacheManager.getCache("bank-holidays")?.clear()
+  }
+
+  @Test
+  fun `gov uk web client is unauthenticated`() {
+    val holidaysDto = HolidaysDto(HolidayEventByDivisionDto(division = "england-and-wales", emptyList()))
+    govUkMockServer.stubGetBankHolidays(holidaysDto)
+
+    val response = govUKHolidayClient.getHolidays()
+
+    assertThat(response).isEqualTo(holidaysDto)
+    govUkMockServer.verify(
+      WireMock.getRequestedFor(WireMock.urlEqualTo(HOLIDAYS_JSON))
+        .withHeader(HttpHeaders.AUTHORIZATION, WireMock.absent()),
+    )
+  }
+}
