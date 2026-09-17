626924292122a605468e8769bb4d157c13d773be
Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
2026-09-17 12:29:47 +0100

    Vb-6919 - add age restriction fields to SessionScheduleDto. (#804)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageprisonvisitsorchestration/dto/visit/scheduler/SessionTemplateDto.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsmanageprisonvisitsorchestration.dto.visit.scheduler
-
-import io.swagger.v3.oas.annotations.media.Schema
-
-data class SessionTemplateDto(
-  @param:Schema(description = "Reference", example = "v9d.7ed.7u", required = true)
-  val reference: String,
-)
